447b01e635731faa4dc58660084358938435d02e
Glen Wojnar <112520930+glwo@users.noreply.github.com>
2023-11-21 10:03:59 -0700

    Solved 'max value' (.js)

---
+++ max_value/max_value.js
@@ -0,0 +1,15 @@
+const maxValue = (nums) => {
+  let max = -Infinity;
+  
+  for(let num of nums){
+    if (num > max){
+      max = num;
+    }
+  }
+  return max;
+};
+​
+module.exports = {
+  maxValue,
+};
+​
