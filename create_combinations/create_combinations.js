96bf819707e140670c3427800afe67f887887c87
Glen Wojnar <112520930+glwo@users.noreply.github.com>
2023-11-26 21:03:06 -0700

    Solved 'create combinations' (.js)

---
+++ create_combinations/create_combinations.js
@@ -0,0 +1,20 @@
+const createCombinations = (items, k) => {
+  // todo
+  if(items.length < k) return [];
+  if(k === 0) return [[]];
+  
+  const first = items[0];
+  
+  const partialCombos = createCombinations(items.slice(1), k - 1);
+  const combosWithFirst = [];
+  for(let combo of partialCombos){
+    combosWithFirst.push([ first, ...combo]);
+  }
+  const combosWithoutFirst = createCombinations(items.slice(1), k);
+  return [...combosWithFirst, ...combosWithoutFirst];
+};
+​
+module.exports = {
+  createCombinations,
+};
+​
