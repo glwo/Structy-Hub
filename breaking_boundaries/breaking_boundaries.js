b9f229c5a91d8780e2eb62ca33223b6d153513d5
Glen Wojnar <112520930+glwo@users.noreply.github.com>
2023-11-29 10:54:26 -0700

    Solved 'breaking boundaries' (.js)

---
+++ breaking_boundaries/breaking_boundaries.js
@@ -0,0 +1,26 @@
+const breakingBoundaries = (m, n, k, r, c, memo = {}) => {
+  const key = `${k}, ${r}, ${c}`
+  
+  if(key in memo) return memo[key];
+  
+  const rowInbounds = 0 <= r && r < m;
+  const colInbounds = 0 <= c && c < n;
+  
+  if(!rowInbounds || !colInbounds) return 1;
+  
+  if(k === 0) return 0;
+  
+  let totalCount = 0;
+  const deltas = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+  for(let delta of deltas){
+    const [dRow, dCol] = delta;
+    totalCount += breakingBoundaries(m, n, k - 1, r + dRow, c + dCol, memo);
+  }
+  memo[key] = totalCount
+  return totalCount;
+};
+​
+module.exports = {
+  breakingBoundaries,
+};
+​
