a8fddad726202b1c1ec0edd729573ecb4dfc4861
Glen Wojnar <112520930+glwo@users.noreply.github.com>
2023-12-01 15:49:36 -0700

    Solved 'uncompress' (.cpp)

--- uncompress/uncompress.cpp
+++ uncompress/uncompress.cpp
@@ -4,29 +4,27 @@
 ​
 std::string uncompress(std::string s) {
   // todo
-   int l = 0;
-    int r = 0;
-    std::string newStr = "";
-​
-    while (r < s.length()) {
-        std::string num = "";
-        while (isdigit(s[r])) {
-            num += s[r];
-            r++;
-        }
-​
-        std::cout << num << std::endl;
-​
-        int currInt = std::stoi(num);
-        for (int i = 0; i < currInt; i++) {
-            newStr += s[r];
-        }
-​
-        l = r + 1;
-        r++;
+  int l = 0;
+  int r = 0;
+  std::string newStr = "";
+  
+  while (r < s.length()){
+    std::string num = "";
+    while(isdigit(s[r])){
+      num += s[r];
+      r++;
     }
-​
-    return newStr;
+    
+    std::cout << num << std::endl;
+    
+    int currInt = std::stoi(num);
+    for(int i = 0; i < currInt; i++){
+      newStr += s[r];
+    }
+    l = r + 1;
+    r++;
+  }
+  return newStr;
 }
 ​
 ​
