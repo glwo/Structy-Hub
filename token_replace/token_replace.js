e00f28d377b8673a0c8c1adeb2ef1a496e88918c
Glen Wojnar <112520930+glwo@users.noreply.github.com>
2023-11-30 20:28:52 -0700

    Solved 'token replace' (.js)

---
+++ token_replace/token_replace.js
@@ -0,0 +1,27 @@
+const tokenReplace = (s, tokens) => {
+  // todo
+  let output = "";
+  let i = 0;
+  let j = 1;
+  
+  while(i < s.length) {
+    if(s[i] !== "$"){
+      output += s[i];
+      i += 1;
+      j = i + 1;
+    } else if (s[j] !== "$"){
+      j += 1;
+    } else {
+      const key = s.slice(i, j + 1);
+      output += tokens[key];
+      i = j + 1;
+      j = i + 1;
+    }
+  }
+  return output;
+};
+​
+module.exports = {
+  tokenReplace,
+};
+​
