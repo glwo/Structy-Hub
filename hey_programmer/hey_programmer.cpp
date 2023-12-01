2b9c336ee6b3db5e0638a065db2dbdacbd45990c
Glen Wojnar <112520930+glwo@users.noreply.github.com>
2023-11-30 20:38:56 -0700

    Solved 'hey programmer' (.cpp)

---
+++ hey_programmer/hey_programmer.cpp
@@ -0,0 +1,12 @@
+#include <string>
+​
+std::string greet(std::string s) {
+  // todo
+  return "hey " + s;
+}
+​
+​
+void run() {
+  // this function behaves as `main()` for the 'run' command
+  // you may sandbox in this function, but should not remove it
+}
