9b08143496a84ad0eab2733e5c67593118a7c8d5
Glen Wojnar <112520930+glwo@users.noreply.github.com>
2023-12-05 12:48:24 -0700

    Solved 'tree path finder' (.cpp)

--- tree_path_finder/tree_path_finder.cpp
+++ tree_path_finder/tree_path_finder.cpp
@@ -1,27 +1,3 @@
-​
-class Node {
-  public:
-    std::string val;
-    Node* left;
-    Node* right;
-​
-​
-    Node(std::string initialVal) {
-      val = initialVal;
-      left = nullptr;
-      right = nullptr;
-    }
-};
-​
-​
-​
-#include <vector>
-#include <string>
-#include <algorithm>
-​
-std::vector<std::string>* _pathFinder(Node* root, std::string targetVal) {
-  if (root == nullptr) {
-    return nullptr;
   }
   if (root->val == targetVal) {
     return new std::vector<std::string>({ root->val });
